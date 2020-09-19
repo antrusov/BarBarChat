57e4f6fd332960a67e10ccc7906606ba32acac2e
antrusov <andrey.trusov.udsu@yandex.ru>
2020-09-19 12:41:44 +0400

    [add] класс ConsoleEx и метод ConsoleEx.Print

---
+++ Library/ConsoleEx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Library
+{
+    public class ConsoleEx
+    {
+        public static void Print(int x, int y, string msg)
+        {
+            Console.CursorLeft = x;
+            Console.CursorTop = y;
+            Console.Write(msg);
+        }
+    }
+}
